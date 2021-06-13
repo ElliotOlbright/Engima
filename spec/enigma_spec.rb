db8bb5c09db57eff2f19c6273c5f0ed8c06861d8
Elliot Olbright <elliotolbright@gmail.com>
2021-06-12 23:32:06 -0600

    start enigma test

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require './lib/offset_generator'
+require './lib/enigma'
+require './lib/key_generator'
+require 'simplecov'
+SimpleCov.start
+
+RSpec.describe Enigma do
+  
+  
+  
+  
+end 
+  
