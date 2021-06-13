25882f02e3f75443c0aea92bcc4a61d7fde2e0c4
Elliot Olbright <elliotolbright@gmail.com>
2021-06-13 10:48:28 -0600

    Add enigma object creation tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,9 +6,18 @@ require 'simplecov'
 SimpleCov.start
 
 RSpec.describe Enigma do
-  
-  
-  
-  
+  before :each do 
+    @enigma = Enigma.new
+  end
+
+  describe 'Object Creation' do 
+    it 'exists' do 
+      expect(@enigma).to be_a(Enigma)
+    end
+
+    it 'has an alphabet' do 
+      expect(@enigma.alphabet.length).to eq(27)
+    end 
+  end 
 end 
   
