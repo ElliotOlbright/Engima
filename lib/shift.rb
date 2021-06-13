fe4f507758988149a9a408b69b0022ce9082986a
Elliot Olbright <elliotolbright@gmail.com>
2021-06-12 23:27:45 -0600

    add attr_reader and refactor

--- lib/shift.rb
+++ lib/shift.rb
@@ -2,13 +2,14 @@ require './lib/offset_generator'
 require './lib/key_generator'
 
 class Shift
-  attr_reader :key
+  attr_reader :key, :offset, :a_shift, :b_shift, :c_shift, :d_shift
+
   def initialize(key, date)
-    @key = KeyGenerator.new(key)
-    @offset = OffsetGenerator.new(date)
-    @a_shift = key.a + offset.a
-    @b_shift = key.b + offset.b
-    @c_shift = key.c + offset.c
-    @d_shift = key.d + offset.d
+    @key     = KeyGenerator.new(key)
+    @offset  = OffSetGenerator.new(date)
+    @a_shift = @key.a + @offset.a
+    @b_shift = @key.b + @offset.b
+    @c_shift = @key.c + @offset.c
+    @d_shift = @key.d + @offset.d
   end 
 end 
