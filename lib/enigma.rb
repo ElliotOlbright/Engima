f40762bca4cb9e3d9822edee7e5e31179534dc32
Elliot Olbright <elliotolbright@gmail.com>
2021-06-14 08:59:54 -0600

    start #encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,18 @@
 class Enigma 
+
   attr_reader :alphabet
   def initialize
     @alphabet = ("a".."z").to_a << " "
   end 
+
+  def encrypt(message, key, date)
+    @shift = Shift.new(key, date)
+    encrypt = []
+    message.chars.each_with_index do |character, index|
+      require "pry"; binding.pry
+    
+    end 
+  end 
+
+
 end 
