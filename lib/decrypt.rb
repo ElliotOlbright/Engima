a01657614b7fa8ae240f1850ac425b7c82cdace0
Elliot Olbright <elliotolbright@gmail.com>
2021-06-15 10:26:24 -0600

    dencryption error fix

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -8,6 +8,6 @@ date = ARGV[3]
 message_to_decrypt = File.read(file_to_decrypt)
 enigma = Enigma.new
 decryption = enigma.decrypt(message_to_decrypt, key, date)
-File.open(decrypted_file, "w") { |file| file.write(decryption[:decryption]) }
+File.open(decrypted_file, "w") { |file| file.write(decryption[:dencryption]) }
 
 p "Created '#{decrypted_file}' with the key #{decryption[:key]} and date #{decryption[:date]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,9 +2,11 @@ require './lib/enigma'
 
 file_to_encrypt = ARGV[0]
 encrypted_file = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
 message_to_encrypt = File.read(file_to_encrypt)
 enigma = Enigma.new
-encryption = enigma.encrypt(message_to_encrypt)
+encryption = enigma.encrypt(message_to_encrypt, key, date)
 File.open(encrypted_file, "w") {|file| file.write(encryption[:encryption]) }
 
 p "Created '#{encrypted_file}' with the key #{encryption[:key]} and date #{encryption[:date]}"
